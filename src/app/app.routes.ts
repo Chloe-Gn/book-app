22a0bd95d0c919eb8ecab815c347c32c799e0dea
Chloe-Gn <chloe.gn29@gmail.com>
2025-04-01 13:13:32 +0200

    fix routing for categories

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LibraryHomepageComponent } from './pages/library-homepage/library-homepage.component';
 import { EditorialSelectionComponent } from './pages/editorial-selection/editorial-selection.component';
+import { BookPageComponent } from './pages/book-page/book-page.component';
 
 export const routes: Routes = [
   {
@@ -12,7 +13,7 @@ export const routes: Routes = [
     component: EditorialSelectionComponent,
   },
   {
-    path: ':category',
+    path: 'categorie/:category',
     component: EditorialSelectionComponent,
   },
 ];
--- src/app/components/4.tile-list/tile-list/tile-list.component.ts
+++ src/app/components/4.tile-list/tile-list/tile-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { TileListPlainTileComponent } from '../tile-list-plain-tile/tile-list-plain-tile.component';
 import { Category } from '../../../models/category.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tile-list',
-  imports: [TileListPlainTileComponent],
+  imports: [TileListPlainTileComponent, RouterLink],
   templateUrl: './tile-list.component.html',
   styleUrl: './tile-list.component.scss',
 })
